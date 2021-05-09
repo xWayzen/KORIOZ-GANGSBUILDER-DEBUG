f96cad636984fd11d8fdefba1d90be4c38bb0ffa
xWayzen <74829014+xWayzen@users.noreply.github.com>
2021-05-09 02:25:23 +0200

    Add files via upload
    Korioz gangs builder debug by xWayzen

---
+++ GangsBuilders/config.lua
@@ -0,0 +1,59 @@
+Config = {}
+Config.Locale = 'fr'
+
+Config.DrawDistance = 25.0
+Config.MarkerType = 1
+Config.MarkerSize = vector3(1.5, 1.5, 1.0)
+Config.MarkerColor = vector3(110, 110, 250)
+
+Config.Uniforms = {
+	robbery_wear = {
+		male = {
+			['helmet_1'] = -1, ['helmet_2'] = 0,
+			['glasses_1'] = 0, ['glasses_2'] = 5,
+			['torso_1'] = 53, ['torso_2'] = 0,
+			['tshirt_1'] = 122, ['tshirt_2'] = 0,
+			['arms'] = 17,
+			['pants_1'] = 37, ['pants_2'] = 0,
+			['bproof_1'] = 9,  ['bproof_2'] = 3,
+			['shoes_1'] = 24, ['shoes_2'] = 0,
+			['helmet_1'] = -1,  ['helmet_2'] = 0,
+			['chain_1'] = 0,    ['chain_2'] = 0,
+			['mask_1'] = 0,    ['mask_2'] = 0,
+			['ears_1'] = -1,     ['ears_2'] = 0,
+			['bags_1'] = 44,     ['bags_2'] = 0
+		}
+	},
+	bullet_wear = {
+		male = {
+			['bproof_1'] = 9, ['bproof_2'] = 3
+		},
+		female = {
+			['bproof_1'] = 13, ['bproof_2'] = 1
+		}
+	},
+	bullet_wear1 = {
+		male = {
+			['bproof_1'] = 0, ['bproof_2'] = 0
+		},
+		female = {
+			['bproof_1'] = 0, ['bproof_2'] = 0
+		}
+	},
+	sac_wear = {
+		male = {
+			['bags_1'] = 44, ['bags_2'] = 0
+		},
+		female = {
+			['bags_1'] = 44, ['bags_2'] = 0
+		}
+	},
+	sac_wear1 = {
+		male = {
+			['bags_1'] = 0, ['bags_2'] = 0
+		},
+		female = {
+			['bags_1'] = 0, ['bags_2'] = 0
+		}
+	}
+}
---
+++ GangsBuilders/server/builder.lua
@@ -0,0 +1,81 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+GangsData = {}
+
+function GetGangs()
+	local data = LoadResourceFile('GangsBuilder', 'data/gangData.json')
+	return data and json.decode(data) or {}
+end
+
+function GetGang(job2)
+	for i = 1, #GangsData, 1 do
+		if job2.name == GangsData[i].Name then
+			return GangsData[i]
+		end
+	end
+
+	return false
+end
+
+ESX.RegisterServerCallback('KorioZ-GangsBuilder:Admin_getUsergroup', function(source, cb)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local plyGroup = xPlayer.getGroup()
+
+	if plyGroup ~= nil then 
+		cb(plyGroup)
+	else
+		cb('user')
+	end
+end)
+
+RegisterServerEvent('gb:addGang')
+AddEventHandler('gb:addGang', function(data)
+	MySQL.Async.execute([[
+INSERT INTO `addon_account` (name, label, shared) VALUES (@gangSociety, @gangLabel, 1);
+INSERT INTO `datastore` (name, label, shared) VALUES (@gangSociety, @gangLabel, 1);
+INSERT INTO `addon_inventory` (name, label, shared) VALUES (@gangSociety, @gangLabel, 1);
+INSERT INTO `jobs2` (`name`, `label`) VALUES (@gangName, @gangLabel);
+INSERT INTO `job2_grades` (`job2_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
+	(@gangName, 0, 'rookie', 'Associé', 0, '{}', '{}'),
+	(@gangName, 1, 'member', 'Soldat', 0, '{}', '{}'),
+	(@gangName, 2, 'elite', 'Elite', 0, '{}', '{}'),
+	(@gangName, 3, 'lieutenant', 'Lieutenant', 0, '{}', '{}'),
+	(@gangName, 4, 'viceboss', 'Bras Droit', 0, '{}', '{}'),
+	(@gangName, 5, 'boss', 'Patron', 0, '{}', '{}')
+;
+	]], {
+		['@gangName'] = data.Name,
+		['@gangLabel'] = data.Label,
+		['@gangSociety'] = 'society_' .. data.Name
+	}, function(rowsChanged)
+		table.insert(GangsData, data)
+		SaveResourceFile('GangsBuilder', 'data/gangData.json', json.encode(GangsData))
+	end)
+end)
+
+RegisterServerEvent('gb:requestSync')
+AddEventHandler('gb:requestSync', function()
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local plyGang = GetGang(xPlayer.job2)
+	TriggerClientEvent('gb:SyncGang', xPlayer.source, plyGang)
+end)
+
+AddEventHandler('esx:playerLoaded', function(source, xPlayer)
+	local plyGang = GetGang(xPlayer.job2)
+	TriggerClientEvent('gb:SyncGang', source, plyGang)
+end)
+
+AddEventHandler('esx:setJob2', function(source, job2)
+	local plyGang = GetGang(job2)
+	TriggerClientEvent('gb:SyncGang', source, plyGang)
+end)
+
+Citizen.CreateThread(function()
+	GangsData = GetGangs()
+
+	for i = 1, #GangsData, 1 do
+		TriggerEvent('esx_society:registerSociety', GangsData[i].Name, GangsData[i].Label, 'society_' .. GangsData[i].Name, 'society_' .. GangsData[i].Name, 'society_' .. GangsData[i].Name, {type = 'public'})
+	end
+end)
